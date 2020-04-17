6c7602eefceed4ac420544a375df07bc1dda585e
rudra1055 <rudrasenr@gmail.com>
2020-04-17 09:53:00 +0600

    1st commit

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Js New Functions:
+
+Math.sqrt() = find the squre root of a number
+Math.ceil() = redrix number convert into a upper integer value (Ex: 12.6 = 13)
+Math.floor() = redrix number convert into a lower integer value (Ex: 12.6 = 12)
+
---
+++ devisors.js
@@ -0,0 +1,14 @@
+var n = 144;
+var range = Math.floor(Math.sqrt(n));
+var devisors = "";
+
+for (let i = 0; i <= range; i++) {
+    if (n%i == 0) {
+        if (i == n/i) {
+            devisors += i + " " ;
+        }else{
+            devisors += i + " " + (n/i) + " ";
+        }
+    }
+}
+console.log(devisors);
